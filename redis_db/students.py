f904fd9bd419aceaaf8f0f5f1ce8abcb4634ee1f
halisha <vadimkaz.alx@gmail.com>
2022-11-23 22:32:12 +0300

    changed identation

--- redis_db/students.py
+++ redis_db/students.py
@@ -3,7 +3,6 @@ import sys
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
-
 from table import Table
 import random
 import redis
@@ -11,73 +10,79 @@ from neo4j_db.graph import Graph
 
 # docker run -d --name redis-cnt -p 6379:6379 redis
 class Students(Table):
-  __shifrs = set()
+    __shifrs = set()
 
-  def __init__(self, clear=False):
-    self.redis_db = redis.Redis(
-      host='localhost', 
-      port=6379, 
-      db=0, 
-      charset='UTF-8',
-      decode_responses=True
-    )
-    self.graph = Graph()
-    if clear: self.clear()
-
-
-  def get_shifrs(self):
-    return self.__shifrs
+    def __init__(self, clear=False):
+        self.redis_db = redis.Redis(
+          host='localhost',
+          port=6379,
+          db=0,
+          charset='UTF-8',
+          decode_responses=True
+        )
+        self.graph = Graph()
+        if clear:
+            self.clear()
 
 
-  def read(self, key):
-    return self.redis_db.hgetall(key)
+    def get_shifrs(self):
+        return self.__shifrs
 
 
-  def insert(self, name, group_name):
-    key = self.__get_shifr()
-    self.redis_db.hset(key, 'name', name)
-    self.redis_db.hset(key, 'group_name', group_name)
-    self.graph.create_student_node(key, name, group_name)
-    return key
+    def read(self, key):
+        return self.redis_db.hgetall(key)
 
 
-  def update(self, key, new_value):
-    self.redis_db.hmset(key, new_value)
+    def insert(self, name, group_name):
+        key = self.__get_shifr()
+        self.redis_db.hset(key, 'name', name)
+        self.redis_db.hset(key, 'group_name', group_name)
+        self.graph.create_student_node(key, name, group_name)
+        return key
 
 
-  def clear(self):
-    keys = self.redis_db.keys()
-    for key in keys:
-      self.redis_db.delete(key)
+    def update(self, key, new_value):
+        self.redis_db.hmset(key, new_value)
 
 
-  def __get_shifr(self):
-    shifr = [random.choice('АБВГД') for _ in range(3)]
-    shifr.append('-')
-    shifr += [random.choice('0123456789') for _ in range(5)]
-    shifr = ''.join(shifr)
-    if shifr in self.__shifrs:
-      return self.__get_shifr()
-    self.__shifrs.add(shifr)
-    return shifr
+    def clear(self):
+        keys = self.redis_db.keys()
+        for key in keys:
+            self.redis_db.delete(key)
 
 
-  def get_by_group(self, group_name):
-    group = []
-    keys = self.redis_db.keys()
-    for key in keys:
-      student = self.read(key)
-      if student['group_name'] == group_name:
-        group.append(key)
-    return group
+    def __get_shifr(self):
+        shifr = [random.choice('АБВГД') for _ in range(3)]
+        shifr.append('-')
+        shifr += [random.choice('0123456789') for _ in range(5)]
+        shifr = ''.join(shifr)
+        if shifr in self.__shifrs:
+            return self.__get_shifr()
+        self.__shifrs.add(shifr)
+        return shifr
 
 
-  def print_all(self):
-    keys = self.redis_db.keys('*')
-    for key in keys:
-      self.print(key)
+    def get_by_group(self, group_name):
+        group = []
+        keys = self.redis_db.keys()
+        for key in keys:
+            student = self.read(key)
+            if student['group_name'] == group_name:
+                group.append(key)
+        return group
 
 
-  def print(self, key):
-    student = self.read(key)
-    print(key, student)
+    def print_all(self):
+        keys = self.redis_db.keys('*')
+        for key in keys:
+            self.print(key)
+
+
+    def print(self, key):
+        student = self.read(key)
+        print(key, student)
+
+
+if __name__ == "__main__":
+    pass
+
