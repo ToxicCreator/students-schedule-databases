5c87b13a99a52eabcfac5780fd670aa80bcfd047
ToxicCreator <aleks7000bol@gmail.com>
2022-11-24 22:18:59 +0300

    fix: fill schedule

--- fill/fill_manager.py
+++ fill/fill_manager.py
@@ -9,7 +9,7 @@ import random
 #from faker import Faker
 # from neo4j_db.graph import Graph
 from mongo.institutes import Institutes
-from postgresql.shedule import Shedule
+from postgresql.schedule import Schedule
 from postgresql.groups import Groups
 from postgresql.lessons import Lessons
 # from elastic.descriptions import Descriptions
@@ -34,8 +34,10 @@ def fill():
 
     groups = __fill_groups(institutes.get_specialities_codes())
     lessons = __fill_lessons(institutes.get_courses_ids())
-    shedule =  __fill_shedule(institutes, lessons, groups)
+    schedule =  __fill_schedule(institutes, lessons, groups)
+    __fill_students(groups_names=group_names, min=10, max=20)
     # __fill_visits(groups_names)
+
 
 
 
@@ -51,7 +53,7 @@ def __fill_groups(specialities_codes):
         for groups_count in range(random.randint(2, 5)):
             groups.insert(generate_group_name(), id)
     return groups
-    
+
 def __fill_lessons(courses_id: list):
     lessons = Lessons(clear = True)
 
@@ -64,8 +66,8 @@ def __fill_lessons(courses_id: list):
     return lessons
 
 
-def __fill_shedule(institutes, lessons, groups):
-    shedule = Shedule(clear = True)
+def __fill_schedule(institutes, lessons, groups):
+    schedule = Schedule(clear = True)
 
     deps_courses = institutes.get_departments_courses()
     deps_specialities = institutes.get_departments_specialities()
@@ -78,18 +80,32 @@ def __fill_shedule(institutes, lessons, groups):
         for speciality_id in specialities:
             current_groups = groups.read_by_speciality_id(speciality_id)
             current_foreign_courses = random.choices(foreign_courses, k=8)
-            all_lessons = lessons.read_by_course_ids(own_courses + foreign_courses)
+            own_lessons = lessons.read_by_course_ids(own_courses)
+            foreign_lessons = lessons.read_by_course_ids(current_foreign_courses)
 
-            for group in current_groups[0]:
-                
-                 
-            
-            
+            for group in current_groups:
+                group_id = group[0]
+                first_year = random.choice([2017, 2018, 2019, 2020])
 
+                for i in range(len(own_lessons)):
+                    lesson_id = own_lessons[i][0]
+                    date = get_lesson_date(
+                        lesson_number=i,
+                        lessons_count=len(own_lessons),
+                        months_count=45,
+                        first_year=first_year
+                    )
+                    schedule.insert(date, lesson_id, group_id)
 
-
-        
-        
+                for i in range(len(foreign_lessons)):
+                    lesson_id = foreign_lessons[i][0]
+                    date = get_lesson_date(
+                        lesson_number=i,
+                        lessons_count=len(foreign_lessons),
+                        months_count=45,
+                        first_year=first_year
+                    )
+                    schedule.insert(date, lesson_id, group_id)
 
 
 
