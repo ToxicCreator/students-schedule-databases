71ca8af6750074522a34e57b421da32afc37b703
ToxicCreator <aleks7000bol@gmail.com>
2022-12-09 01:57:05 +0300

    Feat: First Request

--- services/MainHub/main.py
+++ services/MainHub/main.py
@@ -1,11 +1,29 @@
 
 import os
+<<<<<<< HEAD
+=======
+from dotenv import load_dotenv
+>>>>>>> 440c1b2 (Feat: First Request)
 from fastapi import FastAPI
 import uvicorn
 from first_request import makeFirstRequest
 
 
+
 app = FastAPI()
+load_dotenv()
+
+ELASTIC_IP = os.getenv('ELASTIC_IP')
+ELASTIC_PORT = os.getenv('ELASTIC_PORT')
+elastic_ip = f'https://{ELASTIC_IP}:{ELASTIC_PORT}/'
+
+POSTGRES_IP = os.getenv('POSTGRES_IP')
+POSTGRES_PORT = os.getenv('POSTGRES_PORT')
+postgres_url = f'https://{POSTGRES_IP}:{POSTGRES_PORT}/'
+
+REDIS_IP = os.getenv('REDIS_IP')
+REDIS_PORT = os.getenv('REDIS_PORT')
+redis_url = f'https://{REDIS_IP}:{REDIS_PORT}/'
 
 
 @app.get('/')
@@ -25,7 +43,6 @@ async def index():
 def makeFirstRequest(start: str, end: str, term: str):
     return makeFirstRequest(start, end, term)
 
-
 @app.get('/make-second-request')
 def makeSecondRequest():
     pass
@@ -34,6 +51,30 @@ def makeSecondRequest():
 @app.get('/make-third-request')
 def makeThirdRequest():
     pass
+
+
+def get_lessons_id_by_description(term: str):
+    url = elastic_ip + 'description'
+    return requests.get(url, params={
+        'term': term
+    })
+
+
+def get_percentage_of_visits(lessons_id, start, end):
+    url = postgres_url + 'percentage-of-visits'
+    postgres_body = {
+        'lessons_id': lessons_id,
+        'start': start,
+        'end': end
+    }
+    return requests.post(url, json=postgres_body)
+
+
+def get_students(students_id):
+    url = redis_url + 'students'
+    return requests.post(url, json={
+        "students_id": students_id
+    })
 
 
 if __name__ == "__main__":
